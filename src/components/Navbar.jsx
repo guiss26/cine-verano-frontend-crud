45ff363b41d967076099f7f068dcd5827398d7fa
guiss26 <guissella584@gmail.com>
2026-02-13 10:52:57 -0800

    feat(navbar): layout and add background iamge to header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import { Film } from 'lucide-react';
 
 export default function Navbar() {
     return (
         <>
-        Este es el navbar
+            <header className="">
+                <nav className='text-white text-center'>
+                    <div className='flex text-white gap-3 items-center justify-center '>
+                        <Film size={50}/>
+                        <h1 className='text-5xl'>Cine de Verano</h1>
+                    </div>
+                    <p className='mt-3'>Disfruta de las mejores películas este verano</p>
+                </nav>
+            </header>
         </>
     )
 }
