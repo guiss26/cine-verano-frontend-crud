5b560aaefd5085e8f7363a8593dd8f27d645afb1
guiss26 <guissella584@gmail.com>
2026-02-13 13:35:56 -0800

    feat(footer): layout and style footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
+import { Github } from 'lucide-react';
 
 export default function Footer() {
     return (
         <>
-        Este es el footer
+            <footer className='text-center py-6 bg-[#141212] text-gray-400'>
+                <p>Cine de Verano 2026 - Disfruta el mejor entretenimiento</p>
+                <p className='flex gap-2 justify-center'>Realizado por Guissella Pérez <a href='https://github.com/guiss26' target='_blank'><Github /> </a></p>
+            </footer>
         </>
     )
 }
