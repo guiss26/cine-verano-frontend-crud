ebbb0249f9621fcb69b860006120de30cd18b981
guiss26 <guissella584@gmail.com>
2026-02-20 11:44:45 -0800

    feat(api): implement GET method in services.jsx

--- src/services.js
+++ src/services.js
@@ -1,8 +1,12 @@
 
+const URL_API_FAKE = 'http://localhost:3000/movies'
 
 //GET
-
-//GET/:id
+export  async function getAllMovies() {
+    const response = await fetch(URL_API_FAKE)
+    if(!response.ok) throw Error("Error al cargar películas")
+        return response.json()
+}
 
 // POST 
 
